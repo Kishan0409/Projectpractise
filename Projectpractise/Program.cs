c24f2b38abc17470c5743c5449c154ab2c930e29
Kishan Rai <raikishan79@gmail.com>
2026-04-17 13:25:47 +0530

    Add project files.

---
+++ Projectpractise/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Projectpractise
+{
+    internal class Program
+    {
+        int a = 10, b = 20, c;
+        void add()
+        {
+            c = a + b;
+            Console.WriteLine(c);
+        }
+
+        void sub()
+        {
+            c = a - b;
+            Console.WriteLine(c);
+
+        }
+        void mul()
+        {
+            c = a * b;
+            Console.WriteLine(c);
+        }
+        void div()
+        {
+            c = a / b;
+            Console.WriteLine(c);
+
+        }
+        static void Main(string[] args)
+        {
+           
+         
+      
+
+        }
+    }
+}
